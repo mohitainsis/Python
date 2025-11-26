7d11c10dea53b4010e017cf13242d838df7e6dbf
mohit <thisismohitstm@gmail.com>
2025-11-26 21:24:53 +0530

    tuple.py

--- Lists/01_lists.py
+++ Lists/01_lists.py
@@ -1,65 +1,73 @@
-# thisislist = [1, 2, 3, 4, 5]
-# print(thisislist)
+# # thisislist = [1, 2, 3, 4, 5]
+# # print(thisislist)
 
-# #getting type of list
-# mylist = ["apple ", "banana", "cherry"]
-# print (type(mylist))
+# # #getting type of list
+# # mylist = ["apple ", "banana", "cherry"]
+# # print (type(mylist))
 
-# #Remove list items
-# a = ["12","apple","banana","mango"]
-# a.remove("banana")
-# print(len(a))
+# # #Remove list items
+# # a = ["12","apple","banana","mango"]
+# # a.remove("banana")
+# # print(len(a))
 
-# #pop method
-# a = ["12","apple","banana","mango"]
-# a.pop(1)
-# print(len(a))
+# # #pop method
+# # a = ["12","apple","banana","mango"]
+# # a.pop(1)
+# # print(len(a))
 
-# #If item exists
-# a = ["12","apple","banana","mango"]
-# if "apple" in a:
-#     print ("yes," "apple is in the list")
+# # #If item exists
+# # a = ["12","apple","banana","mango"]
+# # if "apple" in a:
+# #     print ("yes," "apple is in the list")
 
-# #with
-# a = input("Enter your word: ")
-# if "orange" in a:
-#     print ("yes," "orange is in the list")
+# # #with
+# # a = input("Enter your word: ")
+# # if "orange" in a:
+# #     print ("yes," "orange is in the list")
+# # else:
+# #     print("no","This is not present in the list")
+
+# # b = "hello world"
+# # print(b[2:5])
+
+# # list = ["apple","banana","cherry","pineapple"]
+# # print(list[2:5])
+
+
+# # mylist = ["apple","banana","cherry"]
+# # mylist.append("apple")
+# # print(mylist)
+# # mylist = ["apple","banana","cherry"]
+# # mylist1 = ["mango","cherrry","limejuice","coconut"]
+# # mylist.extend(mylist1)
+# # print(mylist)
+
+# #=--------revision TT
+# thislist = ["apple","banana","mango"]
+# print(type(thislist))
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[1:5])
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# if "apple" in thislist:
+#     print("yes apple is available")
 # else:
-#     print("no","This is not present in the list")
+#     print("no it is not available")
 
-# b = "hello world"
-# print(b[2:5])
+# data = [4, 2, 4, 5, 2, 7, 9, 7]
+# unique = []
+# for x in data:
+#     if x not in unique:
+#         unique.append(x)
 
-# list = ["apple","banana","cherry","pineapple"]
-# print(list[2:5])
+# print("Original:", data)
+# print("Unique:", unique)
 
+list = [1,2,3,5,6]
+list.append(8)
+print(list)
 
-# mylist = ["apple","banana","cherry"]
-# mylist.append("apple")
-# print(mylist)
-# mylist = ["apple","banana","cherry"]
-# mylist1 = ["mango","cherrry","limejuice","coconut"]
-# mylist.extend(mylist1)
-# print(mylist)
-
-#=--------revision TT
-thislist = ["apple","banana","mango"]
-print(type(thislist))
-
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(thislist[1:5])
-
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-if "apple" in thislist:
-    print("yes apple is available")
-else:
-    print("no it is not available")
-
-data = [4, 2, 4, 5, 2, 7, 9, 7]
-unique = []
-for x in data:
-    if x not in unique:
-        unique.append(x)
-
-print("Original:", data)
-print("Unique:", unique)
+list = [5,7,867,53,86]
+list.sort()
+print(list)
--- Tuples/Exercises.py
+++ Tuples/Exercises.py
@@ -1,37 +1,62 @@
-fruits = ("apple", "banana", "cherry")
-print(len(fruits))
+# fruits = ("apple", "banana", "cherry")
+# print(len(fruits))
 
-#2nd
-fruits = ("apple", "banana", "cherry")
-print(fruits[0])
+# #2nd
+# fruits = ("apple", "banana", "cherry")
+# print(fruits[0])
 
-#3rd
-fruits = ("apple", "banana", "cherry")
-del fruits
-print(fruits)
+# #3rd
+# fruits = ("apple", "banana", "cherry")
+# del fruits
+# print(fruits)
 
-#4th
-fruits = ("apple","banana","mango","cherry")
-(x,*y,z) = fruits
-print(y)
-#in this x gets the first and z gets teh last the rest goes to y
+# #4th
+# fruits = ("apple","banana","mango","cherry")
+# (x,*y,z) = fruits
+# print(y)
+# #in this x gets the first and z gets teh last the rest goes to y
 
-#5th
-for x in ("bananna","cherry", "mango"):
-    print(x)
+# #5th
+# for x in ("bananna","cherry", "mango"):
+#     print(x)
 
-#6
-fruits = ("apple", "banana", "cherry")
-i = 0
-while i<len(fruits):
-    print(fruits [i])
-    i = i+1
+# #6
+# fruits = ("apple", "banana", "cherry")
+# i = 0
+# while i<len(fruits):
+#     print(fruits [i])
+#     i = i+1
 
-#7
-tuple1 = (1,2,3,5,6)
-tuple1 = tuple1*2
-print(tuple1)
+# #7
+# tuple1 = (1,2,3,5,6)
+# tuple1 = tuple1*2
+# print(tuple1)
 
-tuple1 = (1,2,3,5,6,76)
-result = tuple(x*2 for x in tuple1)
-print(result)
+# tuple1 = (1,2,3,5,6,76)
+# result = tuple(x*2 for x in tuple1)
+# print(result)
+
+# tup = (1,2,5,6,7)
+# print(len(tup))
+# print(type(tup))
+# print(tup[1:3])
+
+
+'''
+to find any particular value in the tuple
+we can use index method ex..
+'''
+# tuple = ("apple","banana", 67)
+# print(tuple.index("banana"))
+
+#--------question
+movies = []
+movie1st = input("Enter your first  favorite movie:")
+movie2nd = input("Enter your second favorite movie :")
+movie3rd = input("Enter your third favorite movie :")
+
+movies.append(movie1st)
+movies.append(movie2nd)
+movies.append(movie3rd)
+
+print(movies)
