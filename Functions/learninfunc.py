71c7557ee573cf90876d26555c5df7a9acfc0a95
mohit <thisismohitstm@gmail.com>
2025-12-07 23:20:13 +0530

    functions

--- Functions/learninfunc.py
+++ Functions/learninfunc.py
@@ -1,10 +1,20 @@
-def isgreater(a,b):
-    if a>b:
-        print("a is greater")
-    else:
-        print("b is greater or equal to ")
+# def isgreater(a,b):
+#     if a>b:
+#         print("a is greater")
+#     else:
+#         print("b is greater or equal to ")
 
-isgreater(5,6)
-a = 14
-b = 20
-isgreater(a,b)
+# isgreater(5,6)
+# a = 14
+# b = 20
+# isgreater(a,b)   
+
+# def myname(fname,middlename ="john", lname="verma"):
+#     print("hello",fname,middlename,lname)
+
+# myname("ron")
+
+def avg(a,b,c):
+    print((a+b+c)/3)
+
+avg(5,6,7)
