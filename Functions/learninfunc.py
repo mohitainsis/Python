332a9e7e134c343828091bb7877093c1fdeae607
mohit <thisismohitstm@gmail.com>
2025-12-07 23:35:34 +0530

    functions

--- Functions/learninfunc.py
+++ Functions/learninfunc.py
@@ -14,7 +14,15 @@
 
 # myname("ron")
 
-def avg(a,b,c):
-    print((a+b+c)/3)
+# def avg(a,b,c):
+#     print((a+b+c)/3)
 
-avg(5,6,7)
+# avg(5,6,7)
+
+def average(*numbers):
+    sum = 0
+    for i in numbers:
+        sum = sum+i
+    print("Average is: ",sum / len(numbers))
+
+average(5,6,76,8)
