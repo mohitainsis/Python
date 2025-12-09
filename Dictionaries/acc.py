23b097743b80faa5e4bfa5a99da445353b2d170d
mohit <thisismohitstm@gmail.com>
2025-12-09 23:40:47 +0530

    dict,set

--- Dictionaries/acc.py
+++ Dictionaries/acc.py
@@ -1,71 +1,95 @@
-thisdict = {
-    "name": "apple",
-    "color": "red",
-    "code": 1235
-}
-print(thisdict["name"])
+# thisdict = {
+#     "name": "apple",
+#     "color": "red",
+#     "code": 1235
+# }
+# print(thisdict["name"])
 
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964,
-  "year": 2020
-}
-print(thisdict)
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964,
+#   "year": 2020
+# }
+# print(thisdict)
 
-thisdict = {
-    "brand":"Ford",
-    "electric": False,
-    "year": 1983,
-    "colors": ["red","white","blue"]
+# thisdict = {
+#     "brand":"Ford",
+#     "electric": False,
+#     "year": 1983,
+#     "colors": ["red","white","blue"]
 
-}
-print(thisdict)
+# }
+# print(thisdict)
 
-thisdict = dict(age = 22 , name = "arush", rollno = 21)
-print(thisdict)
+# thisdict = dict(age = 22 , name = "arush", rollno = 21)
+# print(thisdict)
 
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-x = thisdict.keys()
-print(x)
-x = thisdict.values()
-print(x)
-thisdict = ["year"] = 2020
-print(thisdict)
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# x = thisdict.keys()
+# print(x)
+# x = thisdict.values()
+# print(x)
+# thisdict = ["year"] = 2020
+# print(thisdict)
 
-car ={"brand": "Ford",
-"model": "Mustang",
-"year": 1964
-}
+# car ={"brand": "Ford",
+# "model": "Mustang",
+# "year": 1964
+# }
 
-x = car.values()
+# x = car.values()
 
-print(x) #before the change
+# print(x) #before the change
 
-car["color"] = "red"
+# car["color"] = "red"
 
-print(x) #after the change
+# print(x) #after the change
 
-car ={"brand": "Ford",
-"model": "Mustang",
-"year": 1964
-}
-x =car.keys(
-)
-car["model"] = "apple"
-print(x)
-if "model" in car:
-    print("yes this exist")
+# car ={"brand": "Ford",
+# "model": "Mustang",
+# "year": 1964
+# }
+# x =car.keys(
+# )
+# car["model"] = "apple"
+# print(x)
+# if "model" in car:
+#     print("yes this exist")
 
-#methods in dictionaries
-'''update()
-pop()
-popitem
-copy
-clear
-fromkeys'''
+# #methods in dictionaries
+# '''update()
+# pop()
+# popitem
+# copy
+# clear
+# fromkeys'''
 
+# dict ={"name":"rohan","age":25,"location":"delhii"}
+# print(dict["age"])
+# print(dict.get("name1"))
+# print(dict.keys())
+
+# for key,value in dict.items():
+#     print(f"the value corresponding to {key} is {value}")
+
+# for a,b in dict.items():
+#     print(f"the value of key {a} is {b}")
+
+# dict.popitem()
+
+# del dict
+# print(dict)
+
+i = 0
+while i<10:
+    print(i)
+    i +=1
+    if i==5:
+        break
+else:
+    print("sorry no i found")
--- Sets/join.py
+++ Sets/join.py
@@ -1,67 +1,75 @@
-'''join methods
-union adn update() = join all items from both sides
-intersection() = keeps only duplicates
-differnence() = keeps the items from the first set that are not in otherset
-symmeteric_difference() = keeps all items except the duplicates'''
+# '''join methods
+# union adn update() = join all items from both sides
+# intersection() = keeps only duplicates
+# differnence() = keeps the items from the first set that are not in otherset
+# symmeteric_difference() = keeps all items except the duplicates'''
 
-set1 = {"a", "b", "c"}
-set2 = {1, 2, 3}
-set3= set1.union(set2)
-print(set3)
+# set1 = {"a", "b", "c"}
+# set2 = {1, 2, 3}
+# set3= set1.union(set2)
+# print(set3)
 
-set1 = {"a", "b", "c"}
-set2 = {1, 2, 3}
-set3 = set1.intersection(set2)
-print(set3)
+# set1 = {"a", "b", "c"}
+# set2 = {1, 2, 3}
+# set3 = set1.intersection(set2)
+# print(set3)
 
-set1 = {1,2,3,5}
-set2 = {"apple","banana","cherry"}
-set3 = {"John", "Elena"}
-set4 = {"apple", "bananas", "cherry"}
-set5 = set1.union(set2,set3,set4)
-print(set5)
+# set1 = {1,2,3,5}
+# set2 = {"apple","banana","cherry"}
+# set3 = {"John", "Elena"}
+# set4 = {"apple", "bananas", "cherry"}
+# set5 = set1.union(set2,set3,set4)
+# print(set5)
 
-set1 = {1,2,3,5}
-set2 = {"apple","banana","cherry"}
-set3 = {"John", "Elena"}
-set4 = {"apple", "bananas", "cherry"}
-myset = (set1 | set2 | set3 | set4)
-print(myset)
+# set1 = {1,2,3,5}
+# set2 = {"apple","banana","cherry"}
+# set3 = {"John", "Elena"}
+# set4 = {"apple", "bananas", "cherry"}
+# myset = (set1 | set2 | set3 | set4)
+# print(myset)
 
-# | this works same as union is the operator or union
+# # | this works same as union is the operator or union
 
-set2 = {"apple","banana","cherry"}
-set1 = ("apple","bananna","cherry")
-set3 = set2.union(set1)
-print(set3)
+# set2 = {"apple","banana","cherry"}
+# set1 = ("apple","bananna","cherry")
+# set3 = set2.union(set1)
+# print(set3)
 
-#union doesnt change the original set it creates new if youwant to save the
-'''unions does not change the origingal set it returs a new set
-if you want to save changes in existing set use updat()
-otherwise use union but will have to create a new variable to storre the value
-'''
-set1 = {"a", "b" , "c"}
-set2 = {1, 2, 3}
-set1.update(set2)
-print(set1)
+# #union doesnt change the original set it creates new if youwant to save the
+# '''unions does not change the origingal set it returs a new set
+# if you want to save changes in existing set use updat()
+# otherwise use union but will have to create a new variable to storre the value
+# '''
+# set1 = {"a", "b" , "c"}
+# set2 = {1, 2, 3}
+# set1.update(set2)
+# print(set1)
 
-#intersection returns a new set 
-#keep only duplicates
-set1 = {"apple", "banana", "cherry"}
-set2 = {"google", "microsoft", "apple"}
-set3 = set1.intersection(set2)
-print(set3)
+# #intersection returns a new set 
+# #keep only duplicates
+# set1 = {"apple", "banana", "cherry"}
+# set2 = {"google", "microsoft", "apple"}
+# set3 = set1.intersection(set2)
+# print(set3)
 
-set1 = {"apple", "banana", "cherry"}
-set2 = {"google", "microsoft", "apple"}
-set1.intersection_update(set2)
-print(set1)
+# set1 = {"apple", "banana", "cherry"}
+# set2 = {"google", "microsoft", "apple"}
+# set1.intersection_update(set2)
+# print(set1)
 
-#frozenset
-set1 = frozenset({"apple", "banana", "cherry"})
-print(type(set1))
+# #frozenset
+# set1 = frozenset({"apple", "banana", "cherry"})
+# print(type(set1))
 
 
-set1 = {"apple", "banana", "cherry"}
-x = set1.copy()
-print(x)
+# set1 = {"apple", "banana", "cherry"}
+# x = set1.copy()
+# print(x)
+
+cities={"delhi","mumbai","haryana","kolkata","pune"}
+cities2={"pune","shimla","rajkot","malyalam"}
+cities3 = cities.intersection(cities2)
+print(cities3)
+cities.remove("delhi")
+print(cities)
+print(len(cities))
