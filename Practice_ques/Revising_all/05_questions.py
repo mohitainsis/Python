767a4e986860f8c8da1b976cdda2790acb0efe06
mohit <thisismohitstm@gmail.com>
2025-12-20 16:42:46 +0530

    changed name

---
+++ Practice_ques/Revising_all/05_questions.py
@@ -0,0 +1,32 @@
+#square root of the number()
+#multiply by itself gives square
+#and to see square root have to undo multiplication which can be done by 
+# n to the power one half........8**0.5
+
+# number = int(input("Enter your number "))
+# square_root = number**0.5
+# print(square_root)
+
+
+# import math
+# num = 65
+# squ = math.sqrt(num)
+# print(squ)
+
+#--swapping the numbers
+
+x = 56
+y = 88
+
+temp = x #(56)
+
+x = y
+print("The value of x ", x)
+
+y = temp
+print("The value of y ",y)  #(56)
+
+
+# x =y 
+# y =x
+# print(x,y)
